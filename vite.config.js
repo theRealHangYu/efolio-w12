c8d89806c677e4ba843aacb296706e004d91570c
Hang Yu <hyuu0031@student.monash.edu>
2024-10-15 17:27:28 +1100

    push

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
-export default {
+export default defineConfig({
+  plugins: [vue()],
   base: process.env.NODE_ENV === "production" ? "/efolio-w12/" : "/",
-};
+});
